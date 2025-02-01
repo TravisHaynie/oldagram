082fa608075034a5d0663acf333f4e03448edd86
unknown <travihay@gmail.com>
2025-01-31 20:35:24 -0600

    added code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
+let likeEl = document.getElementById("like-el");
+let btnEl = document.getElementById("btn-el");
+let commentEl = document.getElementById("comment-el");
+let counter = 0;
+const postSection = document.getElementById("post-section");
 const posts = [
     {
         name: "Vincent van Gogh",
         username: "vincey1853",
         location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
+        avatar: "img/vangogh.jpg",
+        post: "img/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
         likes: 21
     },
@@ -12,8 +17,8 @@ const posts = [
         name: "Gustave Courbet",
         username: "gus1819",
         location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
+        avatar: "img/avatar-courbet.jpg",
+        post: "img/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
@@ -21,9 +26,42 @@ const posts = [
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
+        avatar: "img/avatar-ducreux.jpg",
+        post: "img/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
 ]
+  
+
+function renderAllPostsToThePage() {
+ 
+
+    for(let i = 0; i < posts.length; i++) {
+        postSection.innerHTML += `  <div class="flex-container-post">
+                                        <div>
+                                        <img class="post-creator" src=${posts[i].avatar}> 
+                                        </div>
+                                        <div>
+                                        <p class="posted-name">${posts[i].name}</p>
+                                        <p class="posted-destination">${posts[i].location}</p>
+                                        </div>
+                                    </div>
+                                    <div class="vangogh-post-img ">
+                                        <button class="posted-img"><img class="posted-img" src=${posts[i].post}></button>
+                                    <div>`
+                                    
+       likeEl.innerHTML += `${posts[i].likes}`
+      commentEl.innerHTML += `${posts[i].comment}`
+    }
+}
+
+renderAllPostsToThePage()
+
+postSection.addEventListener("click" ,function() {
+
+    console.log(counter)
+    counter ++;
+    likeEl.textContent = counter;
+    
+})
